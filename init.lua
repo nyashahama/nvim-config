bdaec1eb5c1ff1b61443876b043309fc42ad9edb
Nyasha Hama <nyashahama45@gmail.com>
2026-02-03 01:44:05 +0200

    removed lightline.vim dependency

--- init.lua
+++ init.lua
@@ -314,7 +314,34 @@ require("lazy").setup({
   },
   
   -- UI
-  { "itchyny/lightline.vim" },
+  {
+  "nvim-lualine/lualine.nvim",  -- Modern alternative to lightline
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "gruvbox",
+        icons_enabled = true,
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {},
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { {
+          'filename',
+          file_status = true,
+          path = 1,
+        } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      extensions = { 'fzf', 'fugitive', 'oil' },
+    })
+  end
+},
   { "nvim-tree/nvim-web-devicons", opts = {} },
   
   -- Keymap discovery
